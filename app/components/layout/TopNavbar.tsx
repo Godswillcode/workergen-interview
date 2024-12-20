441575c0541b3322c9131670a5ffd258bd9749ef
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2024-12-20 14:50:01 +0100

    icons

--- app/components/layout/TopNavbar.tsx
+++ app/components/layout/TopNavbar.tsx
@@ -1,10 +1,10 @@
 
-
+import { BiSolidGrid } from "react-icons/bi";
 export const TopNavbar = () => {
   return (
     <div className="bg-[#000F28] py-[10px] px-2 text-sm w-full flex justify-between items-center text-gray-300">
       <div className="flex items-center gap-2">
-      <i className="ri-layout-grid-2-fill"></i>
+      <BiSolidGrid />
         <span>Dynamics 365</span>
         <span>|</span>
         <span>Sales hub</span>
