a9daf83a6b2bbae99954125711929b82fc03ac39
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2024-12-21 22:15:57 +0100

    shifted icon

--- app/components/layout/TopNavbar.tsx
+++ app/components/layout/TopNavbar.tsx
@@ -1,16 +1,16 @@
 
-import { BiSolidGrid } from "react-icons/bi";
+import { BsGrid3X3Gap } from "react-icons/bs";
 export const TopNavbar = () => {
   return (
-    <div className="bg-[#000F28] py-[10px] px-2 text-sm w-full flex justify-between items-center text-gray-300">
-      <div className="flex items-center gap-2">
-      <BiSolidGrid />
-        <span>Dynamics 365</span>
-        <span>|</span>
+    <div className="bg-[#000F28] py-[10px] px-2 text-[12px] w-full flex justify-between items-center text-gray-300">
+      <div className="flex items-center gap-[6px]">
+      <BsGrid3X3Gap />
+        <span className="ml-2">Dynamics 365</span>
+        <span className="text-gray-400">|</span>
         <span>Sales hub</span>
       </div>
       <div>
-
+  
       </div>
     </div>
   )
