7315b92125601051cce91117d605062b36fa22a7
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2024-12-21 22:23:23 +0100

    icons

--- app/components/layout/TopNavbar.tsx
+++ app/components/layout/TopNavbar.tsx
@@ -1,5 +1,10 @@
 
 import { BsGrid3X3Gap } from "react-icons/bs";
+import { HiOutlineLightBulb } from "react-icons/hi2";
+import { FaPlus } from "react-icons/fa6";
+import { FiSettings } from "react-icons/fi";
+import { FaQuestion, FaChalkboardUser } from "react-icons/fa6";
+
 export const TopNavbar = () => {
   return (
     <div className="bg-[#000F28] py-[10px] px-2 text-[12px] w-full flex justify-between items-center text-gray-300">
@@ -9,8 +14,13 @@ export const TopNavbar = () => {
         <span className="text-gray-400">|</span>
         <span>Sales hub</span>
       </div>
-      <div>
-  
+      <div className="flex items-center gap-[6px]">
+      <HiOutlineLightBulb />
+      <FaPlus />
+      <FiSettings />
+      <FaQuestion />
+      <FaChalkboardUser />
+      <img src="" alt="user" />
       </div>
     </div>
   )
