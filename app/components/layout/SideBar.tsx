718dc3ab1620b3bcbfbe2d419e435a8341b1ee61
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2024-12-22 07:44:18 +0100

    sidebar

--- app/components/layout/SideBar.tsx
+++ app/components/layout/SideBar.tsx
@@ -1,9 +1,23 @@
 import { SidebarProps } from "@/app/types";
-
+import { VscMenu } from "react-icons/vsc";
+import { AiOutlineHome } from "react-icons/ai";
+import "./style.css";
 
 export const SideBar = ({ isOpen, setIsOpen }: SidebarProps) => {
-    const toggle = () => setIsOpen(!isOpen);
+  const toggle = () => setIsOpen(!isOpen);
   return (
-    <div>SideBar</div>
-  )
-}
+    <div
+      className={`h-screen px-2 pb-5 pt-3 bg-[#EFEFEF] border-r text-[#2C445D] sidebar ${
+        isOpen ? "w-[227px]" : "w-[65px]"
+      }`}
+    >
+      <VscMenu />
+
+      <div className="navLinkWrap mt-4">
+        <div className="navLink">
+          <AiOutlineHome className="navIcon" /> <span>Home</span>
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/components/layout/TopNavbar.tsx
+++ app/components/layout/TopNavbar.tsx
@@ -1,28 +1,27 @@
-
 import { BsGrid3X3Gap } from "react-icons/bs";
 import { HiOutlineLightBulb } from "react-icons/hi2";
 import { FiSettings } from "react-icons/fi";
 import { FaQuestion, FaChalkboardUser, FaPlus } from "react-icons/fa6";
-import userAvatar from "../../images/topNavUser.png"
+import userAvatar from "../../images/topNavUser.png";
 import Image from "next/image";
 
 export const TopNavbar = () => {
   return (
     <div className="bg-[#000F28] py-[10px] px-2 text-[12px] w-full flex justify-between items-center text-gray-300">
       <div className="flex items-center gap-[6px]">
-      <BsGrid3X3Gap />
+        <BsGrid3X3Gap />
         <span className="ml-2">Dynamics 365</span>
         <span className="text-gray-400">|</span>
         <span>Sales hub</span>
       </div>
       <div className="flex items-center gap-4">
-      <HiOutlineLightBulb />
-      <FaPlus />
-      <FiSettings />
-      <FaQuestion />
-      <FaChalkboardUser />
-      <Image src={userAvatar} alt="" className="h-5 w-5"/>
+        <HiOutlineLightBulb />
+        <FaPlus />
+        <FiSettings />
+        <FaQuestion />
+        <FaChalkboardUser />
+        <Image src={userAvatar} alt="" className="h-5 w-5" />
       </div>
     </div>
-  )
-}
+  );
+};
