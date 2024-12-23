0f3c15f1c85d479530683e67b7c5912689103889
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2024-12-23 09:52:16 +0100

    image

--- app/components/LeadDetails.tsx
+++ app/components/LeadDetails.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
 import { leadProps } from "../constant";
 import { IModalProp } from "../types";
 import { Modal } from "antd";
+import titleLog from ".././images/leadDetailsTitle.png";
 
 interface IProps extends IModalProp {
   data?: leadProps;
@@ -18,10 +20,29 @@ export const LeadDetails = ({ open, handleClose, data }: IProps) => {
         borderRadius: "8px",
         padding: "2px",
         background: "linear-gradient(to right, #9BA5FF, #ce8178, #62C4C3)",
+        fontSize: "10px",
       }}
-      title=""
+      title={
+        <div className="flex items-center gap-2">
+          <Image src={titleLog} alt="logo" className="h-4 w-4" />
+          <span className="text-sm">Engage with {data?.name}</span>
+        </div>
+      }
     >
-      <h3>Hello</h3>
+      <div className="">
+        <div className="flex items-center gap-2">
+          <Image
+            src={data?.userImage ?? ""}
+            alt={data?.name ?? ""}
+            className="rounded-full"
+            width={6} height={6}
+          />
+          <div>
+            <h4 className="font-semibold">Jane Reyes</h4>
+            <p className="text-gray-500">COO - Northwind Traders</p>
+          </div>
+        </div>
+      </div>
     </Modal>
   );
 };
