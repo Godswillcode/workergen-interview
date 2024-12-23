a38220920cb024c86ed04a9917758595e4e8c750
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2024-12-23 12:42:20 +0100

    source

--- app/components/LeadDetails.tsx
+++ app/components/LeadDetails.tsx
@@ -7,6 +7,12 @@ import userAvatar from ".././images/userAvatar.png";
 import linkedInLogo from ".././images/linkedinLogo.png";
 import { BsStars } from "react-icons/bs";
 import { MdOutlineModeEdit } from "react-icons/md";
+import { AiOutlineSend } from "react-icons/ai";
+import value from ".././images/value.png";
+import decision from ".././images/decision.png";
+import intent from ".././images/intent.png";
+import lock from ".././images/lock.png";
+import saleLogo from ".././images/salesLogo.png";
 
 interface IProps extends IModalProp {
   data?: leadProps;
@@ -25,7 +31,7 @@ export const LeadDetails = ({ open, handleClose, data }: IProps) => {
         background: "linear-gradient(to right, #9BA5FF, #ce8178, #62C4C3)",
         fontSize: "10px",
       }}
-      width="42rem"
+      width="45rem"
       title={
         <div className="flex items-center gap-2">
           <Image src={titleLog} alt="logo" className="h-4 w-4" />
@@ -33,7 +39,7 @@ export const LeadDetails = ({ open, handleClose, data }: IProps) => {
         </div>
       }
     >
-      <div className="text-[12px] mt-5">
+      <div className="text-[11px] mt-5">
         <div className="">
           <div className="flex rounded-md shadow p-2 items-center gap-2">
             <Image
@@ -45,24 +51,135 @@ export const LeadDetails = ({ open, handleClose, data }: IProps) => {
             />
             <div>
               <h4 className="font-semibold text-[12px]">{data?.name}</h4>
-              <div className="flex items-center gap-[6px]"> <Image src={linkedInLogo} alt="linkedin" className="h-3 w-3"/> |
-              <p className="text-gray-500 text-[10px]">{data?.position}</p>
+              <div className="flex items-center gap-[6px]">
+                {" "}
+                <Image
+                  src={linkedInLogo}
+                  alt="linkedin"
+                  className="h-3 w-3"
+                />{" "}
+                |<p className="text-gray-500 text-[10px]">{data?.position}</p>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="rounded-md flex items-center justify-between bg-gradient-to-r from-[#E9F0FD] to-[#ECECFE] p-2 mt-4">
-           <div className="flex items-center gap-2">
-           <BsStars color="#325BC9" />
-            <span className="gradient-text">{data?.name} may be interested in upgrading machine for the store</span>
-           </div>
+        <div className="rounded-md flex  gap-y-3 flex-col sm:flex-row  justify-between bg-gradient-to-r from-[#E9F0FD] to-[#ECECFE] p-2 mt-4">
+          <div className="flex items-center gap-2">
+            <BsStars color="#325BC9" />
+            <span className="gradient-text">
+              {data?.name} may be interested in upgrading machine for the store
+            </span>
+          </div>
 
-           <div className="flex items-center gap-2">
-              <button className="flex group items-center gap-[4px] hover:bg-[#49495e] rounded border bg-white px-2"><MdOutlineModeEdit className="group-hover:text-white"/> <span className="gradient-text">Edit</span></button>
+          <div className="flex items-center gap-2">
+            <button className="flex group items-center gap-[4px] hover:bg-[#49495e] rounded border bg-white px-2">
+              <MdOutlineModeEdit className="group-hover:text-white" />{" "}
+              <span className="gradient-text">Edit</span>
+            </button>
 
-              <button className="flex group items-center gap-[4px] rounded border px-2"> <span>Approve and send</span></button>
-           </div>
+            <button className="flex group pb-[2px] text-white bg-gradient-to-r from-[#325BC9] to-[#8049D2] group items-center gap-[4px] rounded border px-2">
+              <AiOutlineSend />{" "}
+              <span className="group-hover:text-black">Approve and send</span>
+            </button>
+          </div>
+        </div>
+
+        <div className="rounded mt-3 pt-3 flex items-center gap-3 px-2 border-b shadow-sm">
+          <span className="pb-[10px]">Engage</span>
+          <div className="border-b-2 pb-2 border-[#3E70BF]">Research</div>
+        </div>
+
+        <div className="bg-white rounded p-2 border mt-5">
+          <div className="bg-[#F1F5FF] rounded pl-3 pb-2">
+            <div className="flex justify-end">
+              <BsStars
+                color="#325BC9"
+                className="bg-white -mr-1 h-6 w-6 rounded-full flex items-center justify-center"
+              />
+            </div>
+            <h3 className="-mt-3 font-semibold gradient-text">
+              Why I picked this lead
+            </h3>
+
+            <ul className="mt-2 list-disc ml-5">
+              <li>
+                <span>
+                  {data?.name} is a
+                  <span className="font-medium">key decision maker</span> and
+                  was browsing{" "}
+                  <span className="font-medium">'espresso machines'</span> on
+                  First Coffe's website.
+                </span>
+                <span className="border bg-white rounded px-1 ml-2">1</span>
+              </li>
+              <li>
+                <span>
+                  Multiple people at the company have reported 'slowness' during{" "}
+                  <span className="font-medium">Service request</span>{" "}
+                </span>
+                <span className="border bg-white rounded px-1 ml-2">2</span>
+              </li>
+              <li>
+                <span>
+                  {data?.position} currently sees{" "}
+                  <span className="font-medium">
+                    ${data?.revenueAmount}m in revenue{" "}
+                  </span>
+                  from their in store coffee shops.
+                </span>{" "}
+                <span className="border bg-white rounded px-1 ml-2">3</span>
+              </li>
+            </ul>
+
+            <div className="flex flex-wrap gap-2 mt-4">
+              <div className="border flex gap-3 items-center bg-white rounded-md pl-2 pr-3 py-2">
+                <Image src={decision} className="h-10 w-10" alt="decision" />
+                <div>
+                  <p>Decision maker</p>
+                  <h3 className="font-semibold text-[14px] gradient-text">
+                    Yes
+                  </h3>
+                </div>
+              </div>
+              <div className="border flex gap-3 items-center bg-white rounded-md pl-2 pr-3 py-2">
+                <Image src={value} className="h-10 w-10" alt="decision" />
+                <div>
+                  <p>Potential deal value</p>
+                  <h3 className="font-semibold text-[14px] gradient-text">
+                    $1m
+                  </h3>
+                </div>
+              </div>
+              <div className="border flex gap-3 items-center bg-white rounded-md pl-2 pr-10 py-2">
+                <Image src={intent} className="h-10 w-10" alt="decision" />
+                <div>
+                  <p>Intent</p>
+                  <h3 className="font-semibold text-[14px] gradient-text">
+                    High
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-between mt-3">
+               <div className="flex items-center gap-2">
+                <div className="rounded border p-1 bg-[#F1F5FF]">
+                  <Image src={lock} alt="lock" className="h-4 w-4"/>
+                </div>
+                  <button className="rounded gap-2 flex items-center border py-[2.5px] px-[6px] bg-[#F1F5FF]">
+                    <span>1</span>
+                    <Image src={saleLogo} alt="sales" className="h-4 w-4"/>
+                    <span>D365 Sales</span>
+                  </button>
+                <button className="rounded border py-[2.5px] px-[6px] bg-[#F1F5FF]">+2</button>
+               </div>
+
+               <div>
+                last
+               </div>
+          </div>
         </div>
       </div>
     </Modal>
